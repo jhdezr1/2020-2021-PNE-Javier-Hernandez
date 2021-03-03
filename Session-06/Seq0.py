2600e8fe6a48cede72e3cd2bf8b0900c5eb3dc3f
jhdezr1 <j.hernandezr.2020@alumnos.urjc.es>
2021-03-03 16:09:36 +0100

    session-06

---
+++ Session-06/Seq0.py
@@ -0,0 +1,54 @@
+from pathlib import Path
+import operator
+
+def seq_ping():
+    print('Ok')
+
+def take_out_first_line(seq):
+    return seq[seq.find('\n') + 1:].replace('\n', '')
+
+def seq_read_fasta(filename):
+    return take_out_first_line(Path(filename).read_text())
+
+def seq_len(seq):
+    return len(seq)
+
+def seq_count_base(sequence, base):
+    return sequence.count(base)
+
+def seq_count(sequence):
+    a, c, g, t = 0, 0, 0, 0
+    for d in sequence:
+        if d == 'A':
+            a += 1
+        elif d == 'C':
+            c += 1
+        elif d == 'G':
+            g += 1
+        elif d == 'T':
+            t += 1
+    return {'A': a, 'C': c, 'G': g, 'T': t}
+
+def seq_reverse(sequence):
+    empty = ''
+    for e in sequence:
+        rever_seq = e + empty
+        empty = rever_seq
+    return rever_seq
+
+def seq_complement(sequence):
+    empty_seq = ''
+    for e in sequence:
+        if e == 'A':
+            f = empty_seq + 'T'
+            empty_seq = f
+        elif e == 'C':
+            f = empty_seq + 'G'
+            empty_seq = f
+        elif e == 'T':
+            f = empty_seq + 'A'
+            empty_seq = f
+        elif e == 'G':
+            f = empty_seq + 'C'
+            empty_seq = f
+    return f
