130cf313b4c8991d78c037b4f80f96565e854abc
jhdezr1 <j.hernandezr.2020@alumnos.urjc.es>
2021-03-10 17:32:10 +0100

    session 9-03-21

---
+++ P1/Ex5.py
@@ -0,0 +1,9 @@
+from seq01 import Seq
+
+def print_count(a, c, t, g):
+    print('A: ' +  str(a) + 'C: ' + str(c) + 'T: ' + str(t) + 'G: ' + str(g))
+
+print('-----|Exercise 5|-----')
+s1 = Seq()
+s2 = Seq('ACTGA')
+s3 = Seq('Invalid Sequence')
--- P1/seq01.py
+++ P1/seq01.py
@@ -2,11 +2,15 @@ import termcolor
 
 class Seq:
     """A class for representing sequences"""
-    def __init__(self, strbases = 'NULL'):
+    NULL_SEQUENCE = 'NULL'
+    INVALID_SEQUENCE = 'ERROR'
+
+    def __init__(self, strbases = NULL_SEQUENCE):
         #Initialize the sequence with the value passed as argument when creating the object
         #If Attribute Error: 'Seq' obj has no attribute 'strbases', add self.strbases = strbases at the beginning of the def __init__
         self.strbases = strbases
-        if strbases == 'NULL':
+        strbases = self.strbases
+        if strbases == Seq.NULL_SEQUENCE:
             print('Null seq created')
             self.strbases = strbases
         else:
@@ -15,20 +19,23 @@ class Seq:
                 print('New sequence created!')
 
             else:
-                self.strbases = 'Error'
+                self.strbases = Seq.INVALID_SEQUENCE
                 print('INCORRECT sequence detected')
+
+    @staticmethod
+    def is_valid_sequence_2(bases):
+        for i in bases:
+            if i != 'A' and i != 'C' and i != 'G' and i != 'T':
+                return False
+        return True
+
     def is_valid_sequence(self):
         for i in self.strbases:
             if i != 'A' and i != 'C' and i != 'G' and i != 'T':
                 return False
         return True
 
-    @staticmethod
-    def is_valid_sequence_2(bases):
-        for i in strbases:
-            if i != 'A' and i != 'C' and i != 'G' and i != 'T':
-                return False
-        return True
+
     @staticmethod
     def print_seqs(list_sequences):
         for i in range(0, len(list_sequences)):
@@ -47,9 +54,23 @@ class Seq:
         else:
             return len(self.strbases)
 
-    def len(self):
-        """Calculate the length of the sequence"""
-        return len(self.strbases)
+    def count_base(self):
+        A, C, T, G = 0, 0, 0, 0
+        total = 0
+        if self.strbases == Seq.NULL_SEQUENCE or self.strbases == Seq.INVALID_SEQUENCE:
+            return A, C, G, T
+        else:
+            for e in self.strbases:
+                if e == 'A':
+                    A += 1
+                elif e == 'C':
+                    C += 1
+                elif e == 'T':
+                    T += 1
+                elif e == 'G':
+                    G += 1
+                total += 1
+            return A, C, T, G, total
 
 class Gene(Seq):
     """This class is derived from the Seq Class
@@ -72,6 +93,8 @@ class Gene(Seq):
             return "Sequence" + self.strbases + "is not long"
         else:
             return "Sequence" + self.strbases + "is long"
+
+
 
     
 # --- Main program
