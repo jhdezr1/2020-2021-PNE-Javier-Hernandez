752e133c95eb2f37a242dae8ee3f7b8f64f95caa
jhdezr1 <j.hernandezr.2020@alumnos.urjc.es>
2021-04-16 19:59:16 +0200

    P3 complete

--- P3/test-client.py
+++ P3/test-client.py
@@ -4,6 +4,7 @@ PRACTICE = 3
 EXERCISE = 7
 
 print(f"-----| Practice {PRACTICE}, Exercise {EXERCISE} |------")
+list_sequences = ["ACGTAAAAGTTTAAGCGCCAAT", "AGTCCCCCCAAAATTTTGGGGGAATAT", "AGAGAGAGGATTATTATATACTCTTC", "GGGGGGGGGGGTTTTTTTTTAAAAAACCCC", "AAAAAATTTTTCGAAAAAAA"]
 
 IP = "127.0.0.1"
 PORT = 1202
@@ -19,4 +20,47 @@ print(c.talk("OK!"))
 print("* Testing GET...")
 for argument in range(5):
     list_sequences[int(argument)] + '\n'
-    pass
+
+#info
+print("* Testing INFO...")
+
+s = c.talk("INFO ACGTAAAAGTTTAAGCGCCAAT")
+print("To Server:", argument)
+s.send(argument.encode())
+response = s.recv(2048).decode("utf-8")
+response1 = s.recv(2048).decode("uft-8")
+response2 = s.recv(2048).decode("uft-8")
+response3 = s.recv(2048).decode("uft-8")
+response4 = s.recv(2048).decode("uft-8")
+s.close()
+print("From server: " + response + '\n' + response1 + '\n' + response2 + '\n'+ response3 + '\n' + response4 + '\n')
+
+#conp
+print('* Testing COMP...')
+
+s = c.talk("COMP ACGTAAAAGTTTAAGCGCCAAT")
+print("To Server:", argument)
+s.send(argument.encode())
+response = s.recv(2048).decode("utf-8")
+s.close()
+print("From server: " + response)
+
+#rev
+print('* Testing REV...')
+
+s = c.talk("REV ACGTAAAAGTTTAAGCGCCAAT")
+print("To Server:", argument)
+s.send(argument.encode())
+response = s.recv(2048).decode("utf-8")
+s.close()
+print("From server: " + response)
+
+#gene
+print('* Testing GENE...')
+
+s = c.talk("GENE ACGTAAAAGTTTAAGCGCCAAT")
+print("To Server:", argument)
+s.send(argument.encode())
+response = s.recv(2048).decode("utf-8")
+s.close()
+print("From server: " + response)
